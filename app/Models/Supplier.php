ec83ecbd4f25399a122ab267ea6315e32faecab0
Adam Řezáč <rezac.adam@email.cz>
2022-07-11 14:03:26 +0200

    models and migrations repaired

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -10,7 +10,7 @@ class Supplier extends Model
     use HasFactory;
     public function adresses()
         {
-            return $this->hasMany(Adress::class);
+            return $this->hasMany(Address::class);
         }
     public function bankAccounts()
         {
