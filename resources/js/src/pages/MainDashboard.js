0f059bc2d7201e2dbcccfaf33aeec00455c4f60c
Adam Řezáč <rezac.adam@email.cz>
2022-07-12 14:07:53 +0200

    dashboard

--- resources/js/src/pages/MainDashboard.js
+++ resources/js/src/pages/MainDashboard.js
@@ -1,25 +1,27 @@
 // import { use } from "browser-sync";
 import { useEffect, useState } from "react";
 
+
 export default function MainDashboard(){
-    const [supplier, setSupplier] = useState;
+    const [supplier, setSupplier] = useState([]);
     
 
-    // const url = '/invoices/suppliers/12345678' //make the {ico} -> user.ico
-    // const fetchData = async() => {
-    // const resp = await fetch(url);
-    // const data = resp.json();
-    // setSupplier(data)
-    // console.log(supplier)
-    // };
-
-    const url = '/invoices/suppliers/12345678' //make the {ico} -> user.ico
+    const url = '/api/suppliers' //make the {ico} -> user.ico
     const fetchData = async() => {
-    const resp = await axios.get(url);
-    const data = resp.data.json();
+    const resp = await fetch(url);
+    const data = await resp.json();
+    console.log(data)
     setSupplier(data)
     console.log(supplier)
     };
+
+    // const url = '/api/invoices/suppliers/12345678' //make the {ico} -> user.ico
+    // const fetchData = async() => {
+    // const resp = await axios.get(url);
+    // const data = resp.data.json();
+    // setSupplier(data)
+    // console.log(supplier)
+    // };
 
     useEffect(() => {
         fetchData();
