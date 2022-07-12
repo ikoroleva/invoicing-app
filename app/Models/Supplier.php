918d87dc348e3abfe41de2a65c3a1e1058da6a0a
ikoroleva <prsml.ellisson@gmail.com>
2022-07-12 11:35:04 +0200

    registration with supplier

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
+    protected $fillable = ['user_id', 'name', 'email'];
+
     use HasFactory;
     public function addresses()
     {
