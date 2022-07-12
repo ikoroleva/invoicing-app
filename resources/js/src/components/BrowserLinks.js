fc53fe81e4392db6db8ef5db30f3a4fadf6f8ccf
matimi1 <milos.matiaska@gmail.com>
2022-07-12 14:06:36 +0200

    routing

--- resources/js/src/components/BrowserLinks.js
+++ resources/js/src/components/BrowserLinks.js
@@ -12,11 +12,8 @@ const BrowserLinks = () => {
             {
                 user ? <Fragment>
                     <Link to="/about">About</Link>
-<<<<<<< HEAD
                     <Link to="/userdetails">User</Link>
-=======
                     <Link to="/dashboard">Main Dashboard</Link>
->>>>>>> e02e142635a13749eefc608f4defdc79455d8f67
                 </Fragment> : <Fragment>
                     <Link to="/login">Login</Link>
                     <Link to="/register">Register</Link>
