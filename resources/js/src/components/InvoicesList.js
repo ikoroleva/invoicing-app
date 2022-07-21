b9cc02154c132be0779b7f36b0472815cf7384f6
Ilja M <i.meleshkevich@gmail.com>
2022-07-21 12:08:33 +0200

    delete invoice modal added

--- resources/js/src/components/InvoicesList.js
+++ resources/js/src/components/InvoicesList.js
@@ -3,16 +3,18 @@ import axios from "axios";
 import Table from "react-bootstrap/Table";
 import Dropdown from "react-bootstrap/Dropdown";
 import InvoicesContext from "../context/InvoicesContext";
+import DeleteInvoiceModal from "./DeleteInvoiceModal";
 // import DropdownButton from "react-bootstrap/DropdownButton";
 
 export default function InvoicesList() {
     //state to store all  invoices into - for curently logged in user
-    const { invoices, setInvoices } = useContext(InvoicesContext)
+    const { invoices, setInvoices } = useContext(InvoicesContext);
     const [offset, setOffset] = useState(0);
     const [totalCount, setTotalCount] = useState(0);
     const url = `api/invoices/suppliers/allinvoices?offset=${offset}`;
 
-    const [status, setStatus] = useState('new')
+    const [status, setStatus] = useState("new");
+    const [show, setShow] = useState(false);
 
     //currently logged in user
     const fetchData = async () => {
@@ -25,45 +27,41 @@ export default function InvoicesList() {
 
     //use effect hook to fetch the data
     useEffect(() => {
-    fetchData()
-  },[offset, status]);
+        fetchData();
+    }, [offset, status]);
 
     // //update status function - function that will go to /invoices/changestatus route and with PUT method will change the status of invoice
-    // const updateStatus = (value) => {  
-    // }    
+    // const updateStatus = (value) => {
+    // }
     //probably don't need
-    
+
     const toggleStatus = async (id) => {
-
         const res = await axios({
-            url: '/api/invoices/updatestatus/'+id,
-            method: 'put'
-            })
-        
+            url: "/api/invoices/updatestatus/" + id,
+            method: "put",
+        });
 
-        return fetchData()
-    }
+        return fetchData();
+    };
 
     // function to delte invoice after clicking on dropdown menu
     const deleteInvoice = async (id) => {
-            console.log(id)
+        console.log(id);
 
-            await
+        await await axios({
+            url: "/api/invoices/delete/" + id,
+            method: "delete",
+            data: id,
+        });
+        setShow(true);
+        return fetchData();
+    };
 
-            await axios({
-                url: '/api/invoices/delete/'+id,
-                method: 'delete',
-                data: id
-                })
-
-            return fetchData()
-    }
-
-
-console.log(status)
+    console.log(status);
 
     return (
         <div className="table__container">
+            <DeleteInvoiceModal show={show} setShow={setShow} />
             <Table striped bordered hover>
                 <thead>
                     <tr>
@@ -96,7 +94,6 @@ console.log(status)
                                     </Dropdown.Toggle>
 
                                     <Dropdown.Menu>
-
                                         <Dropdown.Item
                                             href={`/invoice-template/${invoice.id}`}
                                         >
@@ -107,10 +104,30 @@ console.log(status)
 
                                         {/* <Dropdown.Item href="#/action-3">Send via EMAIL</Dropdown.Item> */}
 
-                                        <Dropdown.Item onClick={(e) => toggleStatus(e)}><span onClick={()=>toggleStatus(invoice.id)}>Set status {invoice.status !== 'paid' ? 'paid' : 'unpaid'}</span></Dropdown.Item>
+                                        <Dropdown.Item
+                                            onClick={(e) => toggleStatus(e)}
+                                        >
+                                            <span
+                                                onClick={() =>
+                                                    toggleStatus(invoice.id)
+                                                }
+                                            >
+                                                Set status{" "}
+                                                {invoice.status !== "paid"
+                                                    ? "paid"
+                                                    : "unpaid"}
+                                            </span>
+                                        </Dropdown.Item>
 
-                                        <Dropdown.Item ><span onClick={()=>deleteInvoice(invoice.id)}>Delete</span></Dropdown.Item>
-                                        
+                                        <Dropdown.Item>
+                                            <span
+                                                onClick={() =>
+                                                    deleteInvoice(invoice.id)
+                                                }
+                                            >
+                                                Delete
+                                            </span>
+                                        </Dropdown.Item>
                                     </Dropdown.Menu>
                                 </Dropdown>
                             </td>
